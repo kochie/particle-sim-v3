b1516d90cef2fcded530509d90a6d6fe1e4fb2fd
Robert Koch <robert@kochie.io>
2022-01-31 12:27:29 +1100

    creates minimal environment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,32 @@
+// @ts-check
+
 const withPlugins = require("next-compose-plugins");
 const withOffline = require("next-offline");
 
 const plugins = [withOffline];
 
-module.exports = withPlugins(plugins, nextConfiguration);
+
+/**
+@type {import('next').NextConfig}
+**/
+const nextConfig = {
+    async headers() {
+        return [
+          {
+            source: '/',
+            headers: [
+              {
+                key: 'Cross-Origin-Opener-Policy',
+                value: 'same-origin',
+              },
+              {
+                key: 'Cross-Origin-Embedder-Policy',
+                value: 'require-corp',
+              },
+            ],
+          },
+        ]
+    },
+}
+
+module.exports = withPlugins(plugins, nextConfig);
